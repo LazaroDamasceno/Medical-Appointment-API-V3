ee8f31892da3f8d4a186a1017b10709855c12098
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-21 20:48:51 -0300

    Created the entity Customer

---
+++ v3/Customers/Domain/Customer.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using v3.Common;
+using v3.People.Domain;
+
+namespace v3.Customers.Domain;
+
+public class Customer
+{
+    
+    [BsonId]
+    public ObjectId Id { get; private set; } = ObjectId.GenerateNewId();
+    public Address Address { get; set; }
+    public Person Person { get; set; }
+
+    private Customer(Address address, Person person)
+    {
+        Address = address;
+        Person = person;
+    }
+
+    public static Customer Create(Address address, Person person)
+    {
+        return new Customer(address, person);
+    }
+}

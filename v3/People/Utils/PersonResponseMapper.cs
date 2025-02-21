1d108aad43d0fca6b4c1b0a8785ebc9ae4cb1014
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-21 00:47:18 -0300

    Created the util PersonResponseMapper

--- v3/People/Utils/PersonResponseMapper.cs
+++ v3/People/Utils/PersonResponseMapper.cs
@@ -1,6 +1,12 @@
-﻿namespace v3.People.Utils;
+﻿using v3.People.Domain;
+using v3.People.DTOs;
+
+namespace v3.People.Utils;
 
 public class PersonResponseMapper
 {
-    
+    public static PersonResponseDto Map(Person person)
+    {
+        return new PersonResponseDto(person.Id.ToString(), person.FullName());
+    }
 }

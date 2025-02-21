b4d7020901bd6be1fe15c49f11890c6808bed5cd
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-21 20:35:57 -0300

    Created the DTO Address

---
+++ v3/Common/Address.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace v3.Common;
+
+public record Address(
+    [Required]
+    string Street,
+    [Required]
+    string City,
+    [Required]
+    string State,
+    [Required]
+    string PostalCode
+);

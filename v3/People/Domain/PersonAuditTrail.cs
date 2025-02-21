3531a400380137707a36de79510ea33f4bf402a1
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-21 00:46:00 -0300

    Created the entity PersonAuditTrail

--- v3/People/Domain/PersonAuditTrail.cs
+++ v3/People/Domain/PersonAuditTrail.cs
@@ -1,6 +1,22 @@
-﻿namespace v3.People.Domain;
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace v3.People.Domain;
 
 public class PersonAuditTrail
 {
     
+    [BsonId] 
+    public ObjectId Id { get; private set; } = ObjectId.GenerateNewId();
+    public Person Person { get; private set; }
+
+    private PersonAuditTrail(Person person)
+    {
+        Person = person;
+    }
+
+    public static PersonAuditTrail Create(Person person)
+    {
+        return new PersonAuditTrail(person);
+    }
 }

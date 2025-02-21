d60bec375a746292516e5ac1fb69c55d6126ed51
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-21 00:45:39 -0300

    Created the entity Person

--- v3/People/Domain/Person.cs
+++ v3/People/Domain/Person.cs
@@ -1,17 +1,19 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using v3.People.DTOs;
 
-namespace v3.People;
+namespace v3.People.Domain;
 
 public class Person
 {
 
     [BsonId] 
     public ObjectId Id { get; private set; } = ObjectId.GenerateNewId();
-    public string FirstName { get; private set; }
-    public string MiddleName { get; private set; }
-    public string LastName { get; private set; }
-    public string SSN { get; private set; }
+    private string FirstName { get; set; }
+    private string MiddleName { get; set; }
+    private string LastName { get; set; }
+    public DateOnly BirthDate { get; set; }
+    public string Ssn { get; private set; }
     public string Email { get; private set; }
     public string PhoneNumber { get; private set; }
     public string Gender { get; private set; }
@@ -25,4 +27,31 @@ public class Person
         return $"{FirstName} {MiddleName} {LastName}";
     }
 
+    private Person(PersonRegistrationDto registrationDto)
+    {
+        FirstName = registrationDto.FirstName;
+        MiddleName = registrationDto.MiddleName;
+        LastName = registrationDto.LastName;
+        BirthDate = registrationDto.BirthDate;
+        Ssn = registrationDto.Ssn;
+        Email = registrationDto.Email;
+        PhoneNumber = registrationDto.PhoneNumber;
+        Gender = registrationDto.Gender;
+    }
+
+    public static Person Create(PersonRegistrationDto registrationDto)
+    {
+        return new Person(registrationDto);
+    }
+    
+    private void Modify(PersonModificationDto modificationDto)
+    {
+        FirstName = modificationDto.FirstName;
+        MiddleName = modificationDto.MiddleName;
+        LastName = modificationDto.LastName;
+        BirthDate = modificationDto.BirthDate;
+        Email = modificationDto.Email;
+        PhoneNumber = modificationDto.PhoneNumber;
+        Gender = modificationDto.Gender;
+    }
 }

5ecb0082cb90e595ff22ca6ea25bb2e80a198956
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-21 21:48:18 -0300

    Created the DTO CustomerRegistrationDto

---
+++ v3/Customers/DTOs/CustomerRegistrationDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using v3.Common;
+using v3.People.DTOs;
+
+namespace v3.Customers.DTOs;
+
+public record CustomerRegistrationDto(
+    [Required] PersonRegistrationDto PersonRegistrationDto,
+    [Required] Address Address
+);

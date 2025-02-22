71357d8f91a5cde89287e8f14c9d38ba696eccfe
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-21 21:18:10 -0300

    Created the context class MongoDbContext

---
+++ v3/Context/MongoDbContext.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+using v3.People.Domain;
+
+namespace v3.Context;
+
+public class MongoDbContext
+{
+    private IMongoDatabase Database { get; set; }
+    
+    private MongoDbContext(IConfiguration configuration)
+    {
+        var connectionString = configuration.GetConnectionString("DbConnection");
+        var mongoUrl = new MongoUrl(connectionString);
+        var mongoClient = new MongoClient(mongoUrl);
+        Database = mongoClient.GetDatabase(mongoUrl.DatabaseName);   
+    }
+    
+    public IMongoCollection<Person> PeopleCollection => Database.GetCollection<Person>("People");
+    
+    public IMongoCollection<Person> CustomersCollection => Database.GetCollection<Person>("Customers");
+}

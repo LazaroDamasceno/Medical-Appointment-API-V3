c6ac1cefb388034de0fa45365ddcfc7bc9af7731
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-21 21:55:07 -0300

    Created the DTO CustomerResponseDto

---
+++ v3/Customers/DTOs/CustomerResponseDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using v3.Common;
+
+namespace v3.Customers.DTOs;
+
+public record CustomerResponseDto(
+    [Required] string Id, 
+    [Required] string FullName, 
+    [Required] string Address
+);

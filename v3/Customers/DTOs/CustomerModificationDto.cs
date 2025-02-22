f87d3e34dc8190a39b9c074347ce35b6428e6fd4
lazarodamasceno <lazarolawrence2@gmail.co>
2025-02-21 21:49:22 -0300

    Created the DTO CustomerModificationDto

---
+++ v3/Customers/DTOs/CustomerModificationDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using v3.Common;
+using v3.People.DTOs;
+
+namespace v3.Customers.DTOs;
+
+public record CustomerModificationDto(
+    [Required] PersonModificationDto PersonModificationDto,
+    [Required] Address Address
+);

5f7014ec28fd390d1af9688c788289395a5be76f
lazarodamasceno <lazarolawrence2@gmail.co>
2025-03-06 01:41:16 -0300

    Created the exception NonExistentDoctorException

---
+++ v3/Doctors/Exceptions/NonExistentDoctorException.cs
@@ -0,0 +1,4 @@
+﻿namespace v3.Doctors.Exceptions;
+
+public class NonExistentDoctorException(string medicalLicenseNumber)
+    : Exception($"Doctor whose medical license number is {medicalLicenseNumber} was not found.");

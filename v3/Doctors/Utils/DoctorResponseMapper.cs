7b13cd953a610401690844aa3a4839f7786e7656
lazarodamasceno <lazarolawrence2@gmail.co>
2025-03-06 01:40:05 -0300

    Created the util DoctorResponseMapper

---
+++ v3/Doctors/Utils/DoctorResponseMapper.cs
@@ -0,0 +1,15 @@
+﻿using v3.Doctors.Domain;
+using v3.Doctors.DTOs;
+
+namespace v3.Doctors.Utils;
+
+public class DoctorResponseMapper
+{
+    public static DoctorResponseDto Map(Doctor doctor)
+    {
+        return new DoctorResponseDto(
+            doctor.MedicalLicenseNumber,
+            doctor.Person.FullName()
+        );
+    }
+}

21f46b89f0bfb8a6655cd9247989f21cff2299fd
lazarodamasceno <lazarolawrence2@gmail.co>
2025-03-17 20:58:02 -0300

    Created the utility class MedicalSlotResponseMapper

---
+++ v3/MedicalSlots/Utils/MedicalSlotResponseMapper.cs
@@ -0,0 +1,18 @@
+﻿using v3.Doctors.Utils;
+using v3.MedicalSlots.Domain;
+using v3.MedicalSlots.DTOs;
+
+namespace v3.MedicalSlots.Utils;
+
+public static class MedicalSlotResponseMapper
+{
+    public static MedicalSlotResponseDto Map(MedicalSlot medicalSlot)
+    {
+        return new MedicalSlotResponseDto(
+            DoctorResponseMapper.Map(medicalSlot.Doctor),
+            medicalSlot.AvailableAt,
+            medicalSlot.CanceledAt,
+            medicalSlot.CompletedAt
+        );
+    }
+}

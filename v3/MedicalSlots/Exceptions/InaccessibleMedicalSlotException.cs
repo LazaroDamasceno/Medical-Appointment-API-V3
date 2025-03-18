ea6a498ca1c8e9daaa6820de1893726d47644941
lazarodamasceno <lazarolawrence2@gmail.co>
2025-03-18 14:59:58 -0300

    Created the exception InaccessibleMedicalSlotException

---
+++ v3/MedicalSlots/Exceptions/InaccessibleMedicalSlotException.cs
@@ -0,0 +1,4 @@
+﻿namespace v3.MedicalSlots.Exceptions;
+
+public class InaccessibleMedicalSlotException(string medicalLicenseNumber)
+    : Exception($"Doctor whose medical license number is {medicalLicenseNumber} is not medical slot's doctor.");

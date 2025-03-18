d5ba6e1a275b13f8ef7f97114522813ae52ec0c5
lazarodamasceno <lazarolawrence2@gmail.co>
2025-03-17 21:18:55 -0300

    Created the utility class BlockedDateTimeHandler

---
+++ v3/Common/BlockedDateTimeHandler.cs
@@ -0,0 +1,14 @@
+﻿namespace v3.Common;
+
+public static class BlockedDateTimeHandler
+{
+
+    public static void Handle(DateTime dateTime)
+    {
+        if (BlockedDateTimeChecker.IsBeforeToday(dateTime))
+        {
+            const string message = "Given date and time must today or in the future.";
+            throw new BlockedBookingDateTimeException(message);
+        }
+    }
+}

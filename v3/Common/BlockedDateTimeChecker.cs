aa06dde5d2d643714a074075c8058feef3266d86
lazarodamasceno <lazarolawrence2@gmail.co>
2025-03-17 21:15:30 -0300

    Created the utility class BlockedDateTimeChecker

---
+++ v3/Common/BlockedDateTimeChecker.cs
@@ -0,0 +1,13 @@
+﻿namespace v3.Common;
+
+public static class BlockedDateTimeChecker
+{
+    
+    public static bool IsBeforeToday(DateTime dateTime)
+    {
+        var now = DateTime.Now;
+        var isDayLessThanToday = now.Day < dateTime.Day;
+        var areMonthsAndYearsEqual = now.Month == dateTime.Month && now.Year == dateTime.Year;
+        return isDayLessThanToday && areMonthsAndYearsEqual;
+    }
+}
